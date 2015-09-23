6f3b41871702649b87bacbe2b5f498e6dc880cc7
Joel Trottier-Hebert <iamquebecois@gmail.com>
2015-09-23 11:41:25 -0400

    Basic fetching -- every 5 secs it polls new messages (starting from the beginning) and prints them in the console.

--- bin/main.dart
+++ bin/main.dart
@@ -1,8 +1,70 @@
 // Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-import 'package:slack_history_keeper/slack_history_keeper.dart' as slack_history_keeper;
+import 'package:slack_history_keeper/slack_history_keeper.dart'
+    as slack_history_keeper;
+import 'dart:io';
+import 'package:logging/logging.dart';
+import 'dart:async';
 
-main(List<String> arguments) {
-  print('Hello world: ${slack_history_keeper.calculate()}!');
+final Logger log = new Logger('Application');
+
+Future fetchMessages(
+    slack_history_keeper.Channel generalChannel,
+    slack_history_keeper.Message lastMessage,
+    List<slack_history_keeper.Message> messages) {
+  return slack_history_keeper
+      .fetchChannelHistory(generalChannel.id,
+          lastTimestamp: lastMessage != null ? lastMessage.timestamp : '0')
+      .then((List<slack_history_keeper.Message> newMessages) {
+    newMessages.reversed.forEach((slack_history_keeper.Message m) => print(m));
+    messages
+      ..clear()
+      ..addAll(newMessages);
+  });
+}
+
+String checkSlackApiToken() {
+  var slackApitoken = Platform.environment['SLACK_TOKEN'];
+
+  if (slackApitoken == null) {
+    log.severe("SLACK_TOKEN is not a defined environment variable");
+    throw new Exception("SLACK_TOKEN is not defined.");
+  }
+
+  return slackApitoken;
+}
+
+setupLogger() {
+  Logger.root.onRecord.listen((LogRecord rec) {
+    print('${rec.level.name}: ${rec.time}: ${rec.message}');
+  });
+}
+
+main(List<String> arguments) async {
+  setupLogger();
+
+  var slackApitoken = checkSlackApiToken();
+  slack_history_keeper.slackApiToken = slackApitoken;
+
+  var users = await slack_history_keeper.fetchUsers();
+  var channels = await slack_history_keeper.fetchChannels();
+  var generalChannel = channels
+      .firstWhere((slack_history_keeper.Channel c) => c.name == 'general');
+
+  var messages =
+      await slack_history_keeper.fetchChannelHistory(generalChannel.id);
+
+  messages.reversed.forEach((slack_history_keeper.Message m) => print(m));
+
+  new Timer.periodic(new Duration(seconds: 5), (Timer t) {
+    if (messages.isEmpty) {
+      fetchMessages(generalChannel, null, messages);
+    } else {
+      slack_history_keeper.Message lastMessage = messages.first;
+
+      fetchMessages(generalChannel, lastMessage, messages);
+    }
+  });
 }
--- test/slack_history_keeper_test.dart
+++ test/slack_history_keeper_test.dart
@@ -1,13 +1,49 @@
 // Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 library slack_history_keeper.test;
 
 import 'package:slack_history_keeper/slack_history_keeper.dart';
 import 'package:test/test.dart';
+import 'dart:convert';
+import 'dart:io';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  slackApiToken = Platform.environment["SLACK_TOKEN"];
+  String channelId = 'C0AT47NAU'; // general channel
+
+  test('slack api token is specified', () {
+    expect(slackApiToken, isNotNull);
+  });
+
+  test('fetch users should get a list of users containing user joel', () async {
+    User joel = new User()
+      ..id = 'U0AT0BWTE'
+      ..name = 'joel';
+
+    List<User> users = await fetchUsers();
+
+    expect(users[0], equals(joel));
+  });
+
+  test('fetch channels should serialize correctly', () async {
+    Channel general = new Channel()
+      ..id = 'C0AT47NAU'
+      ..name = 'general';
+    Channel random = new Channel()
+      ..id = 'C0AT2UJ6B'
+      ..name = 'random';
+
+    List<Channel> channels = await fetchChannels();
+
+    expect(channels, hasLength(2));
+    expect(channels[0], equals(general));
+    expect(channels[1], equals(random));
+  });
+
+  test('fetch channel history', () async {
+    var history = await fetchChannelHistory(channelId);
+    print(history);
   });
 }
