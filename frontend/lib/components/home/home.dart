3c45e3daa44509be76609a363fb8ab0d2fc36e46
Joel Trottier-Hebert <iamquebecois@gmail.com>
2015-09-30 11:57:15 -0400

    home fetching channels

--- frontend/lib/components/home/home.dart
+++ frontend/lib/components/home/home.dart
@@ -2,17 +2,28 @@ library frontend.components.home;
 
 import 'package:angular2/angular2.dart';
 import 'package:slack_history_keeper_frontend/services/slack_service.dart';
+import 'package:slack_history_keeper_shared/models.dart';
+import 'dart:async';
+import 'dart:html';
 
-@Component(selector: 'home', viewBindings: const [SlackService])
+@Component(
+    selector: 'home',
+    viewBindings: const [SlackService])
 @View(
     templateUrl: 'home.html',
     directives: const [CORE_DIRECTIVES, FORM_DIRECTIVES])
 class Home implements OnInit {
   SlackService service;
+  List<Channel> channels = [];
+  Channel channel;
+
+  Home(this.service) {}
 
   onInit() async {
-    await service.getChannels();
+    channels = await service.getChannels();
   }
 
-  Home(this.service);
+  onChannelChange(event) {
+    channel = channels.firstWhere((Channel c) => c.id == event.target.value);
+  }
 }
--- frontend/lib/services/slack_service.dart
+++ frontend/lib/services/slack_service.dart
@@ -3,14 +3,21 @@ library frontend.services.my_service;
 import 'package:angular2/angular2.dart';
 import 'dart:async';
 import 'package:http/browser_client.dart' as http;
+import 'package:slack_history_keeper_shared/models.dart';
+import 'dart:convert';
+import 'package:redstone_mapper/mapper.dart';
+import 'package:redstone_mapper/mapper_factory.dart';
 
 @Injectable()
 class SlackService {
   String apiUrl = 'http://localhost:8084/api';
   http.BrowserClient client = new http.BrowserClient();
 
-  Future getChannels() async {
+  Future<List<Channel>> getChannels() async {
+    bootstrapMapper();
     var result = await client.get('$apiUrl/channels');
-    print(result.body);
+    List<Map> json = JSON.decode(result.body);
+    print(json);
+    return json.map((Map m) => decode(m, Channel)).toList();
   }
 }
