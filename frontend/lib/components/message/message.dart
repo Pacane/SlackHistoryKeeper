cb6560f4ccd9276a5aba0007602783fd758f2d96
Christopher Viel <viel.christopher@gmail.com>
2015-12-17 09:00:55 -0500

    Synchronize all calls asking the cache

--- frontend/lib/components/message/message.dart
+++ frontend/lib/components/message/message.dart
@@ -2,10 +2,22 @@ library frontend.components.message;
 
 import 'package:angular2/angular2.dart';
 import 'package:slack_history_keeper_shared/models.dart';
+import 'package:slack_history_keeper_frontend/services/slack_service.dart';
+import 'dart:async';
 
 @Component(selector: 'message')
 @View(templateUrl: 'message.html')
-class MessageComponent {
+class MessageComponent implements OnInit {
+  final SlackService slackService;
+
   @Input()
   Message message;
+  User user;
+
+  MessageComponent(this.slackService);
+
+  Future ngOnInit() async {
+    var users = await slackService.getUsers();
+    user = users.firstWhere((u) => u.id == message.userId);
+  }
 }
