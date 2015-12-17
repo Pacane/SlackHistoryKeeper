aead1c036056f1ed563cccc9b80986dbc5d378d7
Maxime Mériouma-Caron <maxime.meriouma@arcbees.com>
2015-12-17 14:21:28 -0500

    Ze loading animation

--- frontend/lib/app.dart
+++ frontend/lib/app.dart
@@ -4,6 +4,10 @@ import 'package:angular2/angular2.dart';
 import 'package:angular2/router.dart';
 
 import 'package:slack_history_keeper_frontend/components/search/search_component.dart';
+import 'dart:async';
+import 'package:event_bus/event_bus.dart';
+import 'package:slack_history_keeper_frontend/events/slack_data_loaded_event.dart';
+import 'dart:html';
 
 @Component(selector: 'app')
 @View(
@@ -11,4 +15,42 @@ import 'package:slack_history_keeper_frontend/components/search/search_component
     directives: const [ROUTER_DIRECTIVES, SearchComponent])
 @RouteConfig(
     const [const Route(path: '/', component: SearchComponent, name: 'Search')])
-class App {}
+class App implements OnDestroy {
+  final EventBus eventBus;
+
+  StreamSubscription subscription;
+
+  bool _loaded;
+  Timer _loadTimer;
+
+  App(this.eventBus) {
+    subscription = eventBus.on(SlackDataLoadedEvent).listen(_onSlackDataLoaded);
+    _loadTimer = new Timer(new Duration(seconds: 4), () {
+      if (_loaded) {
+        _setVisible();
+      }
+    });
+  }
+
+  void _setVisible() {
+    var pageHeader = querySelector(".page-header");
+    pageHeader
+      ..classes.remove("is-loading")
+      ..on["webkitAnimationIteration"]
+          .listen((e) => pageHeader.classes.remove("animate"));
+    querySelector(".page-content").classes.add("is-visible");
+  }
+
+  void _onSlackDataLoaded(SlackDataLoadedEvent event) {
+    _loaded = true;
+    if (_loadTimer.isActive) {
+      _loadTimer.cancel();
+      _setVisible();
+    }
+  }
+
+  @override
+  void ngOnDestroy() {
+    subscription.cancel();
+  }
+}
