7e1c1470ff7461cefd305941f40e204a7aa0dacb
Joel Trottier-Hebert <iamquebecois@gmail.com>
2015-12-17 14:31:28 -0500

    Konami

--- frontend/lib/app.dart
+++ frontend/lib/app.dart
@@ -4,6 +4,9 @@ import 'package:angular2/angular2.dart';
 import 'package:angular2/router.dart';
 
 import 'package:slack_history_keeper_frontend/components/search/search_component.dart';
+import 'package:konami_code/konami_code.dart';
+
+import 'dart:html';
 
 @Component(selector: 'app')
 @View(
@@ -11,4 +14,12 @@ import 'package:slack_history_keeper_frontend/components/search/search_component
     directives: const [ROUTER_DIRECTIVES, SearchComponent])
 @RouteConfig(
     const [const Route(path: '/', component: SearchComponent, name: 'Search')])
-class App {}
+class App implements OnInit {
+  @override
+  ngOnInit() {
+    var konami = new KonamiCode();
+    konami.onPerformed.listen((_) {
+      window.location.replace('http://meatspin.com');
+    });
+  }
+}
