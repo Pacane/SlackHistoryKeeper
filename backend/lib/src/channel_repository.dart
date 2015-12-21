70f6ab7bd2f9e063e3f395cc9f08e962f52bb79e
Joel Trottier-Hebert <iamquebecois@gmail.com>
2015-12-21 13:51:56 -0500

    Extract connection pool

--- backend/lib/src/channel_repository.dart
+++ backend/lib/src/channel_repository.dart
@@ -1,23 +1,18 @@
 import 'dart:async';
 
-import 'package:connection_pool/connection_pool.dart';
 import 'package:di/di.dart';
 import 'package:mongo_dart/mongo_dart.dart';
-import 'package:slack_history_keeper_backend/src/mongo_db_pool.dart';
 import 'package:slack_history_keeper_shared/models.dart';
 import 'package:slack_history_keeper_shared/convert.dart';
+import 'package:slack_history_keeper_backend/src/has_connection_pool.dart';
+import 'package:slack_history_keeper_backend/src/mongo_db_pool.dart';
 
 @Injectable()
-class ChannelRepository {
-  final MongoDbPool connectionPool;
+class ChannelRepository extends HasConnectionPool {
   final ChannelDecoder channelDecoder = new ChannelDecoder();
   final ChannelEncoder channelEncoder = new ChannelEncoder();
 
-  ManagedConnection connection;
-
-  Future<Db> getConnection() async => connection.conn;
-
-  ChannelRepository(this.connectionPool);
+  ChannelRepository(MongoDbPool connectionPool) : super(connectionPool);
 
   Future<List<Channel>> fetchChannels() async {
     var channels = <Channel>[];
@@ -44,18 +39,4 @@ class ChannelRepository {
           writeConcern: new WriteConcern(fsync: true));
     });
   }
-
-  Future executeWrappedCommand(CommandToExecute command) async {
-    try {
-      var connection = await connectionPool.getConnection();
-
-      return await command(connection.conn);
-    } catch (e) {
-      print("Error with database connection: $e}");
-    } finally {
-      connectionPool.releaseConnection(connection);
-    }
-  }
 }
-
-typedef Future CommandToExecute(Db db);
